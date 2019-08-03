4ee6e1ce853cb86ede846bde8951511d4991e475
Jairo Henao <jairo.henao@regoconsulting.com>
2019-08-03 00:46:32 -0500

    Adding Soap service

---
+++ virtual-gourmet-web/src/main/java/com/acme/beans/ServicioSoap.java
@@ -0,0 +1,27 @@
+package com.acme.beans;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.acme.entities.Mesero;
+
+@WebService(serviceName = "virtual-gourmet-soap", targetNamespace = "http//com.acme.virtual-gourmet")
+@Stateless
+public class ServicioSoap {
+
+	@PersistenceContext(unitName = "virtual-gourmet-pu2")
+	private EntityManager em2;
+	
+	@WebMethod(operationName = "listaMeseros")
+	public List<Mesero> obtenerListaMeseros() {
+		Query queryJPQL = em2.createQuery("SELECT ms FROM Mesero ms ORDER BY ms.nombre");
+		
+		return queryJPQL.getResultList();
+	}
+}
