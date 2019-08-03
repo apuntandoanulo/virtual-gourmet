d015ec7e7442a02497e3ff11e6c09f7e89c2008c
Jairo Henao <jairo.henao@regoconsulting.com>
2019-08-03 11:25:54 -0500

    Agregando soporte a pruebas unitarias

--- virtual-gourmet-web/src/main/java/com/acme/beans/ServicioRest.java
+++ virtual-gourmet-web/src/main/java/com/acme/beans/ServicioRest.java
@@ -1,12 +1,15 @@
 package com.acme.beans;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -46,5 +49,31 @@ public class ServicioRest {
 		logger.info("Se encontraron {} registros", meseros.size());
 		
 		return Response.ok(json, MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Consumes({"application/json"})
+	public Response agregarMesero(String body) {
+		logger.debug("Agregando nuevo mesero...");
+		
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.setPrettyPrinting();
+		Gson gson = gsonBuilder.create();
+		
+		Mesero nuevoMesero = gson.fromJson(body, Mesero.class);
+		nuevoMesero.setId(null);
+		nuevoMesero.setFechaCreacion(new Date());
+		
+		try {
+			em2.persist(nuevoMesero);
+			
+			logger.debug("El nuevo mesero ha sido registrado");
+			
+			return Response.ok().build();
+		} catch (Exception ex) {
+			logger.error("Ha ocurrido un error: " + ex.getMessage());
+			
+			return Response.serverError().build();
+		}
 	}
 }
