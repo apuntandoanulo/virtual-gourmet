20733c206ce0478c4c3c0b30fdddfbf9f8ea42fb
Jairo Henao <jairo.henao@regoconsulting.com>
2019-10-25 17:28:24 -0500

    Ejemplo de documentacion con el estandar javadoc

--- java_src/virtual-gourmet-jpa/src/main/java/com/acme/core/ElementoPreparacion.java
+++ java_src/virtual-gourmet-jpa/src/main/java/com/acme/core/ElementoPreparacion.java
@@ -1,6 +1,16 @@
 package com.acme.core;
 
+/**
+ * Interfaz padre de todos los objetos de dominio involucrados en el 
+ * proceso de preparacion
+ * 
+ * @author Jairo Henao
+ *
+ */
 public interface ElementoPreparacion {
 
+	/**
+	 * Accion previa antes de utilizar el elemento de preparacion
+	 */
 	public void alistar();
 }
